61d8c97e3ede85820b79af93cc00346c4b0b5ff7
Joshua Liu <joshua.liu@sagebase.org>
2017-03-15 01:43:36 -0700

    Remove unused files

--- android-sdk/src/main/java/org/sagebionetworks/bridge/android/manager/dao/UploadDAO.java
+++
@@ -1,8 +0,0 @@
-package org.sagebionetworks.bridge.android.manager.dao;
-
-/**
- * Created by jyliu on 3/10/2017.
- */
-
-public class UploadDAO {
-}
--- android-sdk/src/test/java/org/sagebionetworks/bridge/android/data/UploadManagerIntegrationTest.java
+++
@@ -1,8 +0,0 @@
-package org.sagebionetworks.bridge.android.data;
-
-/**
- * Created by jyliu on 3/13/2017.
- */
-
-public class UploadManagerIntegrationTest {
-}
