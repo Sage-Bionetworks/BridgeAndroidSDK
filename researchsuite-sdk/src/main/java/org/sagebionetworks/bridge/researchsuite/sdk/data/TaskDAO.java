394a5f85b69a150a4c72aeed62904915ede50e53
Joshua Liu <liujoshua@users.noreply.github.com>
2017-10-13 17:48:28 -0700

    Bring in RSuite 0.0.3; Test that Room works

---
+++ researchsuite-sdk/src/main/java/org/sagebionetworks/bridge/researchsuite/sdk/data/TaskDAO.java
@@ -0,0 +1,39 @@
+/*
+ *    Copyright 2017 Sage Bionetworks
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package org.sagebionetworks.bridge.researchsuite.sdk.data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+/**
+ * Created by liujoshua on 10/11/2017.
+ */
+@Dao
+public interface TaskDAO {
+    @Query("SELECT * FROM tasks WHERE identifier = :identifier")
+    TaskEntity getById(String identifier);
+
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    void upsert(TaskEntity... TaskEntity);
+
+    @Delete
+    void delete(TaskEntity... TaskEntity);
+}
---
+++ researchsuite-sdk/src/main/java/org/sagebionetworks/bridge/researchsuite/sdk/data/TaskEntity.java
@@ -0,0 +1,40 @@
+/*
+ *    Copyright 2017 Sage Bionetworks
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package org.sagebionetworks.bridge.researchsuite.sdk.data;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by liujoshua on 10/11/2017.
+ */
+@Entity(tableName = "tasks")
+public class TaskEntity {
+    @NonNull
+    @PrimaryKey
+    private String identifier;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+}
